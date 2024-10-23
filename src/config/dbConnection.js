45b37019496ce68cdb25deab89fce84fdb134fda
Kugan <kugapriyanj97@gmail.com>
2024-10-23 22:32:12 +0530

    added db config

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.Mongo_Connect);
+    await mongoose.connect(process.env.Mongo_Connect,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log("db Connected ");
   } catch (error) {
     throw error;
